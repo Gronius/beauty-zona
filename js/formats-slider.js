48b14913bb0cc963af330368cda188e952d96ad9
Vlad <vlivonika@gmail.com>
2024-04-16 07:27:42 +0200

    add responsive sections

--- js/formats-slider.js
+++ js/formats-slider.js
@@ -1,17 +1,19 @@
 const swiper = new Swiper('.swiper-container', {
   loop: true,
   slidesPerView: 1,
-  //   spaceBetween: 20,
+  centre: true,
+    spaceBetween: 20,
   breakpoints: {
     768: {
       slidesPerView: 2,
     },
-    1280: {
+    1200: {
       slidesPerView: 3,
     },
   },
   pagination: {
     el: '.pagination',
+    clickable: true,
     bulletClass: 'pagination__button',
     bulletActiveClass: 'pagination__button--active',
   },
